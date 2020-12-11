84a4965be48240a176f5707437ecde088f6c3e8b
wardhai <46731990+wardhai@users.noreply.github.com>
2020-12-11 17:55:37 -0600

    added select type box

--- ITMD411Final/src/javaapplication1/Login.java
+++ ITMD411Final/src/javaapplication1/Login.java
@@ -1,6 +1,7 @@
 package javaapplication1;
 
 import java.awt.Color;
+
 import java.awt.Font;
 import java.awt.GridLayout; //useful for layouts
 import java.awt.event.ActionEvent;
@@ -14,7 +15,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
-import javax.swing.JTextField;
+import javax.swing.JTextField;   
 
 @SuppressWarnings("serial")
 public class Login extends JFrame {
@@ -27,23 +28,28 @@ public class Login extends JFrame {
 		conn = new Dao();
 		conn.createTables();
 		setSize(400, 210);
-		setLayout(new GridLayout(4, 2));
+		setLayout(new GridLayout(5, 2));
 		setLocationRelativeTo(null); // centers window
 		getContentPane().setBackground(new Color(105, 96, 219));
 		
 		// SET UP CONTROLS
 		JLabel lblUsername = new JLabel("Username", JLabel.LEFT);
 		JLabel lblPassword = new JLabel("Password", JLabel.LEFT);
+		JLabel lblType = new JLabel("Select Type", JLabel.LEFT);
 		JLabel lblStatus = new JLabel(" ", JLabel.CENTER);
 		lblUsername.setForeground(Color.WHITE);
 		lblPassword.setForeground(Color.WHITE);
 		lblStatus.setForeground(Color.WHITE);
+		lblType.setForeground(Color.WHITE);
 	    lblUsername.setFont(new Font("Garamond", Font.PLAIN, 18));
 	    lblPassword.setFont(new Font("Garamond", Font.PLAIN, 18));
+	    lblType.setFont(new Font("Garamond", Font.PLAIN, 18));
 
 		// JLabel lblSpacer = new JLabel(" ", JLabel.CENTER);
 
 		JTextField txtUname = new JTextField(10);
+		
+		JTextField txtType = new JTextField(10);
 
 		JPasswordField txtPassword = new JPasswordField();
 		JButton btn = new JButton("Submit");
@@ -61,15 +67,18 @@ public class Login extends JFrame {
 		lblStatus.setToolTipText("Contact help desk to unlock password");
 		lblUsername.setHorizontalAlignment(JLabel.CENTER);
 		lblPassword.setHorizontalAlignment(JLabel.CENTER);
+		lblType.setHorizontalAlignment(JLabel.CENTER);
  
 		// ADD OBJECTS TO FRAME
 		add(lblUsername);  // 1st row filler
 		add(txtUname);
 		add(lblPassword); // 2nd row
 		add(txtPassword);
-		add(btn);         // 3rd row
+		add(lblType);     // 3rd row
+		add(txtType);	  
+		add(btn);         // 4th row
 		add(btnExit);
-		add(lblStatus);   // 4th row
+		add(lblStatus);   // 5th row
 
 		btn.addActionListener(new ActionListener() {
 			int count = 0; // count agent
@@ -81,10 +90,11 @@ public class Login extends JFrame {
 				count = count + 1;
 				// verify credentials of user (MAKE SURE TO CHANGE TO YOUR TABLE NAME BELOW)
 
-				String query = "SELECT * FROM swifthq_users WHERE uname = ? and upass = ?;";
+				String query = "SELECT * FROM swifthq_users WHERE uname = ? and upass = ? and utype = ?;";
 				try (PreparedStatement stmt = conn.getConnection().prepareStatement(query)) {
 					stmt.setString(1, txtUname.getText());
 					stmt.setString(2, txtPassword.getText());
+					stmt.setString(3, txtType.getText());
 					
 					ResultSet rs = stmt.executeQuery();
 					if (rs.next()) {
@@ -104,6 +114,8 @@ public class Login extends JFrame {
 
 		setVisible(true); // SHOW THE FRAME
 	}
+	
+	//adding combo box for select type (Admin
 
 	public static void main(String[] args) {
 
